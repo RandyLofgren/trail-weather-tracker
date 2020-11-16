2eea3d9196053b7986242e8e6cbeb30bbb839d8f
DimitriNakos <dimi1nakos@gmail.com>
2020-11-16 14:58:43 -0500

    APIs uploaded

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,21 @@
 var APIkey = "200975281-2d283bf1ff307c50113654f42a31551f"
 
 $.ajax({
-    url: "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=" + APIkey,
+    url: "https://www.hikingproject.com/data/get-trails?lat=35.227&lon=-80.843&maxDistance=300&key=" + APIkey,
     method: "GET"
 }).then(function (response) {
+    // console.log(response)
     console.log(response)
+
+
+});
+
+$.ajax({
+    url: "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=93048a14e536394603a5f5173a41d761",
+    method: "GET"
+}).then(function (weather) {
+    // console.log(response)
+    console.log(weather)
+
+
 });
