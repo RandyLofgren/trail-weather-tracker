f132ef305c3198a91a215f4edc98b9f1957f82e0
RandyLofgren <43276499+RandyLofgren@users.noreply.github.com>
2020-11-17 13:57:24 -0500

    for loop for trails working

--- assets/script.js
+++ assets/script.js
@@ -31,7 +31,7 @@ $(".is-info").on("click", function (event) {
         url: "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=Charlotte,NorthCarolina&appid=93048a14e536394603a5f5173a41d761",
         method: "GET"
     }).then(function (weather) {
-        // console.log(response)
+        
         console.log(weather)
 
         let lat = weather.coord.lat
@@ -41,15 +41,19 @@ $(".is-info").on("click", function (event) {
             url: "https://cors-anywhere.herokuapp.com/https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + long + "&maxDistance=30&key=200975281-2d283bf1ff307c50113654f42a31551f",
             method: "GET"
         }).then(function (response) {
-            // console.log(response)
-            $(".trail-name").text(response.trails[0].name)
-            $(".trail-info").text(response.trails[0].summary)
             console.log(response)
+            for (var i = 1; i < response.trails.length; i++) {
+                var nameResp = response.trails[i].name;
+                var infoResp = response.trails[i].summary;
+                $("#name" + i).text(nameResp);
+                $("#info" + i).text(infoResp);
+               
 
 
-        });
 
+            };
 
+        })
 
     });
 })
